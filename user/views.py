7a501b1b5ffcd8fd23cc8889ff8f51bd9d9ecd83
solleepang <dlthf727@gmail.com>
2023-09-04 16:35:49 +0900

    회원가입, 로그인 , 로그아웃 기능

---
+++ user/views.py
@@ -0,0 +1,58 @@
+from django.contrib import auth
+from django.shortcuts import render,redirect
+from .models import UserModel
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model # 사용자가 데이터베이스 안에 있는지 검사하는 함수
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+def sign_up_view(request):
+    if request.method == 'GET':
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        password2 = request.POST.get('password2', '')
+        email = request.POST.get('email', '')
+        bio = request.POST.get('bio', '')
+
+        if password != password2:
+            return render(request, 'user/signup.html')
+        else:
+            exist_user = get_user_model().objects.filter(username=username)
+            if exist_user:
+                return render(request, 'user/signup.html')
+            else:
+                UserModel.objects.create_user(username=username, password=password, email=email, bio=bio)
+                return redirect('/sign-in')
+
+
+def sign_in_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        me = auth.authenticate(request, username=username, password=password)
+        if me is not None:
+            auth.login(request, me)
+            return redirect('/')
+        else:
+            return redirect('/sign-in')
+
+    elif request.method == 'GET':
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signin.html')
+
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
