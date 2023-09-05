d92f05b4828a1e71da09926e7fbcb371bb0e7e01
solleepang <dlthf727@gmail.com>
2023-09-05 22:15:14 +0900

    수정하기 기능 추가

--- tweet/urls.py
+++ tweet/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('tweet/', views.tweet, name='tweet'),
-    path('tweet/delete/<int:id>', views.delete_tweet, name='delete-tweet'),
+    path('tweet/', views.create_tweet, name='tweet'),
     path('tweet/<int:id>', views.detail_tweet, name='detail-tweet'),
     path('tweet/mypage/<int:id>', views.my_tweet, name='my-tweet'),
+    path('tweet/delete/<int:id>', views.delete_tweet, name='delete-tweet'),
+    path('tweet/update/<int:id>', views.update_tweet, name='update-tweet'),
 ]
--- tweet/views.py
+++ tweet/views.py
@@ -11,16 +11,20 @@ def home(request):
         return redirect('/sign-in')
 
 
-def tweet(request):
+def create_tweet(request):
     if request.method == 'GET':
+        # 로그인 확인
         user = request.user.is_authenticated
         if user:
+            # 로그인됨: 모든 할 일 가져와서 보여주기
             all_tweet = TweetModel.objects.all().order_by('-created_at')
             return render(request, 'tweet/home.html', {'tweet': all_tweet})
         else:
+            # 로그인 안 됨:
             return redirect('/sign-in')
 
     elif request.method == 'POST':
+        # 새 할 일 생성
         user = request.user
         my_tweet = TweetModel()
         my_tweet.author = user
@@ -30,7 +34,7 @@ def tweet(request):
         return redirect('/tweet')
 
 
-@login_required
+@login_required # 로그인 한 사람만 접근 가능
 def delete_tweet(request, id):
     my_tweet = TweetModel.objects.get(id=id)
     my_tweet.delete()
@@ -49,3 +53,17 @@ def my_tweet(request, id):
         return render(request, 'tweet/tweet_my.html', {'tweet': my_tweet})
     else:
         return redirect('/')
+
+
+def update_tweet(request, id):
+    tweet = TweetModel.objects.get(id=id)
+    if request.method == 'GET':
+        # 개별 페이지에서 수정 버튼 클릭시 수정 가능한 페이지로 이동
+        return render(request,'tweet/tweet_update.html', {'tweet': tweet})
+
+    elif request.method == 'POST':
+        # 수정 후 수정하기 버튼 클릭시 내용 저장하고 개별페이지로 이동
+        tweet.title = request.POST['my-title']
+        tweet.content = request.POST['my-content']
+        tweet.save()
+        return redirect(f'/tweet/{id}')
